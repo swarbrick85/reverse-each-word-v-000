caf56b5ca1b8af9a1281f55e5349cd26f09159e8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-11 13:39:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(sentence1)
   a = sentence1.split(" ")
+  b = []
   a.collect do |word|
-    word = word.reverse 
+    word = word.reverse
+    b<<word
   end 
-  a.join(" ") 
+  b.join(" ")
 end 
